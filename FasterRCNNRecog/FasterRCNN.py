90ae32fbb1d1336525341e7122c8bbcb744e798e
ZiwenZhuang <leo416850130@outlook.com>
2018-09-05 15:09:01 +0800

    working on proposal layer

--- FasterRCNNRecog/FasterRCNN.py
+++ FasterRCNNRecog/FasterRCNN.py
@@ -6,27 +6,52 @@ import torchvision.datasets as datasets
 import torchvision.models as models
 
 from modules import Conv2d, FC, ROIPool
+import utils
 
 class RPN(nn.module):
 	''' Region Proposal Network as a component of the Faster-rcnn net
-		Using .training field to define the forward() behavior
 	'''
-	def __init__(self):
+	def __init__(self, configs = {
+								"anchor_scales": [8, 16, 32], \
+								"anchor_ratios": [0.5, 1, 2], \
+								"lambda": 10, \
+								}):
+		''' Using configs field to store all the configurations as well as hyper-parameters
+			"lambda": this is the hyper-parameter during calculating the loss
+		'''
 		super.__init__(self, RPN)
-		self.feature_net = models.vgg16() # The covnets that outputs the feature map
-		# Considering the output of vgg is 512 channel, take the input of Conv layer as 512 channel
-		self.conv0 = Conv2d(512, 512, 3, same_padding=True) 
+		self.configs = configs
 
-	def rpn_score(self, x):
+		# The covnets that outputs the feature map
+		self.feature_net = models.vgg16()
+
+		# Considering the output of vgg is 512 channel, take the input of Conv layer as 512 channel
+		self.conv0 = Conv2d(512, 512, 3, same_padding=True)
+
+		num_anchors = len(self.configs["anchor_scales"]) * len(self.configs["anchor_ratios"])
+		self.score_conv = Conv2d(512, num_anchors * 2, 1, relu= False, same_padding= False) # using softmax later, so no ReLU
+		self.bbox_conv = Conv2d(512, num_anchors * 4, 1, relu= False, same_padding= False)
+
+		# Recording the loss
+		self.loss = None
+
+	def rpn_score(self, features):
 		''' It outputs score for each part of the feature map (from the result of self.conv0),
 			for the input of making proposal
 		'''
-		pass
+		score = self.score_conv(features)
+		# Considering Softmax is not a learnable layer, we don't need to "see" it later.
+		return nn.Softmax(1)(score)
 
-	def rpn_bbox(self, x):
+	def rpn_bbox(self, features):
 		''' Proposing bounding boxes for selecting the proposals.
 		'''
-		pass
+		return self.bbox_conv(features)
+
+	def proposal_layer(self, rpn_conv, rpn_bbox):
+		''' For the simplicity, the implementation is moved to another file.
+		'''
+		return utils.proposal_layer(rpn_conv, rpn_bbox)
 
 	def forward(self, x):
 		assert isinstance(x, torch.Tensor) | isinstance(x, torch.Variable)
@@ -38,6 +63,8 @@ class RPN(nn.module):
 
 		rpn_prob = self.rpn_score(rpn_conv)
 		rpn_bbox_pred = self.rpn_bbox(rpn_conv)
+		#	Until now the tensors are always (N, C, H, W), where C is always the number of output
+		# for each pixel in the feature map.
 
 		# another output item: roi using the proposal layer
 		rois = self.proposal_layer(...)
--- FasterRCNNRecog/utils.py
+++ FasterRCNNRecog/utils.py
@@ -7,7 +7,7 @@ def set_trainable(model, require_grad = True):
 	for param in model.parameters():
 		param.requires_grad = require_grad
 
-def generate_anchor(img, stride = 1, scales = [128, 256, 512], ratios = [0.5, 1, 2]):
+def generate_anchor(img_info, stride = 1, scales = [8, 16, 32], ratios = [0.5, 1, 2]):
 	'''	Generating the 4 parameters (x1, y1, x2, y2) of each anchor.
 		And concatenate them along the 0-th dimension.
 		---------------------------------
@@ -16,10 +16,11 @@ def generate_anchor(img, stride = 1, scales = [128, 256, 512], ratios = [0.5, 1,
 		scales: the width of anchor (when it is a square)                     |          |
 		ratios: under certain scale, the ratio between width and height       +-------x2, y2
 		---------------------------------
-		output: a tensor (H*W/stride/stride, 4) a series of anchor parameters, for the memory efficiency
+		output: a tensor (H*W/stride/stride, 4) a series of anchor parameters, for the memory
+			efficiency
 	'''
-	_num_anchors = (img.shape()[-2] // stride) * (img.shape()[-1] // stride)
-	x_ctr, y_ctr = torch.meshgrid([img.shape()[2] // stride, img.shape()[3] // stride])
+	_num_anchors = (img_info[-2] // stride) * (img_info[-1] // stride)
+	x_ctr, y_ctr = torch.meshgrid([img_info[2] // stride, img_info[3] // stride])
 	# keep the array into one dimension, (2 dimension in all)
 	x_ctr = x_ctr.reshape((-1, 1)).expand(_num_anchors * len(scales) * len(ratios), 1)
 	y_ctr = y_ctr.reshape((-1, 1)).expand(_num_anchors * len(scales) * len(ratios), 1)
@@ -36,10 +37,10 @@ def generate_anchor(img, stride = 1, scales = [128, 256, 512], ratios = [0.5, 1,
 	for scale in scales:
 		for ratio in ratios:
 			h_seq.append(
-				torch.round(torch.sqrt(torch.mul(base, scale * scale * ratio)))
+				torch.round(torch.sqrt(torch.mul( base, int(float(scale * scale) * float(ratio)) )))
 				)
 			w_seq.append(
-				torch.round(torch.sqrt(torch.mul(base, scale * scale / ratio)))
+				torch.round(torch.sqrt(torch.mul( base, int(float(scale * scale) * float(ratio)) )))
 				)
 	h_half = torch.round(torch.mul(torch.cat(h_seq, 0).reshape((-1, 1)), 0.5))
 	w_half = torch.round(torch.mul(torch.cat(w_seq, 0).reshape((-1, 1)), 0.5))
@@ -54,3 +55,31 @@ def generate_anchor(img, stride = 1, scales = [128, 256, 512], ratios = [0.5, 1,
 	anchors = torch.cat(anchors, 1).contiguous()
 
 	return anchors
+
+def proposal_layer(rpn_score, rpn_bbox, img_info, anchor_scales, anchor_ratios, nms_thresh= 0.7):
+	'''	Extract useful proposals that are both high in score and big in area.
+		---------------------
+		Parameters
+		----------
+		rpn_score: ...
+		rpn_bbox: the network predicted slight shift for the "ground true" bounding box relative to
+			the anchor at the specific point.
+		img_info: the [height, width] of the input image (I means the input image of the whole network)
+		anchor_scales: the anchor size if put onto the input images
+		anchor_ratios: the ratio between the height and the width of the anchor
+		nms_thresh: Non-Maximum Suppression threshold
+		----------------------
+		Returns
+		----------
+		rpn_rois: a batch of bunch of rois that are elected from the network proposals.
+			(N, H * W * A, 5) where H and W are the size of input feature map (rpn_score, rpn_bbox)
+			and A is the number of anchors for each point in the feature map
+	'''
+	#	Considering the output proposal for each image is different, you should provide batch with
+	# only 1 item, or it is difficult for this method to return the same amount of proposals for
+	# the whole batch.
+	
+	assert rpn_score.size()[0] == 1 & rpn_rpn_bbox.size()[0] == 1, \
+		"Only single item batches are supported"
+	
+
